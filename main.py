e5fcd62562ef0cd12b974bb7651e568ed8a1b374
BrandonBudhan <22506515-BrandonBudhan@users.noreply.replit.com>
2023-09-01 16:23:54 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# === Flashcard Program! ===
+
+def create_card():
+    response = input("Would you like to create a flashcard?(y/n)  ")
+
+    while response != 'y' and response != 'n':
+        print("Please response with 'y' or 'n'")
+        response = input("Would you like to create a flashcard? (y/n)")
+    
+    if response == 'n':
+        return
+    
+
+create_card()
